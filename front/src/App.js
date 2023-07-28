0898e9378e9fb01f55495d58fc7522e0c6f02ffb
amisztal <alejoemisztal07@gmail.com>
2023-07-28 14:16:35 -0300

    Template

---
+++ front/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Componente } from './components/Componente';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <Componente/>
+    </div>
+  );
+}
+
+export default App;
---
+++ front/src/components/Componente.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export const Componente = () => {
+
+  return (
+    <div>
+      asdasdaf
+    </div>
+  )
+}
