d60ba5365b969aebb79d543c9314ac6a107846a6
amisztal <alejoemisztal07@gmail.com>
2023-07-29 15:11:06 -0300

    Banda superior finalizada

---
+++ challenge/src/components/Account.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import '../stylesheets/Accounts.css'
+
+export default function Account() {
+  return (
+    <div className='Accounts'>
+        Account
+    </div>
+  )
+}
---
+++ challenge/src/components/AccountsContainer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import '../stylesheets/Accounts.css'
+import Account from './Account'
+
+export const AccountsContainer = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
